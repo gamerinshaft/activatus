e9d465785355e6fdceec2645e5213bc92a634343
j.taneichi <j.tane3ma@gmail.com>
2015-12-22 00:09:20 +0900

    update

--- src/activatus.js
+++ src/activatus.js
@@ -1,13 +1,5 @@
 export class Activatus{
-  canActivate(params, routeConfig, navigationInstruction) {
-    console.log("======================================================================")
-    console.log("Activatus!")
-    console.log("======================================================================")
-    console.log("params: ")
-    console.log(params)
-    console.log("routeConfig: ")
-    console.log(routeConfig)
-    console.log("navigationInstruction: ")
-    console.log(navigationInstruction)
+  canActivate(params, routeConfig, navigationInstruction){
+
   }
 }
